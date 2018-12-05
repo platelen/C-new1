a26d33118f0fe4bbb15e1ae4a136dddcc5e561aa
platelen <platelen@gmail.com>
2018-12-05 21:15:34 +0300

    Сделал два класс на горизон и вертикаль.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -14,42 +14,14 @@ namespace Snake
             Point p1 = new Point(1,3,'*');
             p1.Draw();
 
-            Point p2 = new Point(4,5,'*');
+            Point p2 = new Point(4,5,'#');
             p2.Draw();
 
-            Point p3 = new Point(7, 4, '*');
-            p3.Draw();
+            HorizontalLine line = new HorizontalLine(5,10,8,'+');
+            line.Drow();
 
-            Point p4 = new Point(2, 7, '*');
-            p4.Draw();
-
-
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.Remove(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-
-            List<Point> myPoint = new List<Point>();
-            myPoint.Add(p1);
-            myPoint.Add(p2);
-            myPoint.Add(p3);
-            myPoint.Add(p4);
-
+            VerticalLine vline = new VerticalLine(7, 10, 12, '*');
+            vline.Drow();
 
             Console.ReadLine();
 
