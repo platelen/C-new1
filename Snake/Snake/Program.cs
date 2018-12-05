1cb556b66f64ce5dd9abcab49e54996b85ca1dfa
platelen <platelen@gmail.com>
2018-12-05 20:47:05 +0300

    Добавил свой список точек

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -14,13 +14,13 @@ namespace Snake
             Point p1 = new Point(1,3,'*');
             p1.Draw();
 
-            Point p2 = new Point(4,5,'#');
+            Point p2 = new Point(4,5,'*');
             p2.Draw();
 
-            Point p3 = new Point(7, 4, '@');
+            Point p3 = new Point(7, 4, '*');
             p3.Draw();
 
-            Point p4 = new Point(2, 7, '%');
+            Point p4 = new Point(2, 7, '*');
             p4.Draw();
 
 
@@ -43,9 +43,13 @@ namespace Snake
             List<Point> pList = new List<Point>();
             pList.Add(p1);
             pList.Add(p2);
-            pList.Add(p3);
-            pList.Add(p4);
-            
+
+
+            List<Point> myPoint = new List<Point>();
+            myPoint.Add(p1);
+            myPoint.Add(p2);
+            myPoint.Add(p3);
+            myPoint.Add(p4);
 
             Console.ReadLine();
 
