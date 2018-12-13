641fbd97a6bb3f9e0bd93fd404f953cd79b6d6a5
platelen <platelen@gmail.com>
2018-12-13 20:20:24 +0300

    Реализация звука

--- GuessMelody/GuessMelody/fMessage.Designer.cs
+++ GuessMelody/GuessMelody/fMessage.Designer.cs
@@ -28,16 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.lbMessage = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbTimer = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lbMessage
             // 
             this.lbMessage.AutoSize = true;
             this.lbMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lbMessage.Location = new System.Drawing.Point(97, 80);
+            this.lbMessage.Location = new System.Drawing.Point(51, 52);
             this.lbMessage.Name = "lbMessage";
             this.lbMessage.Size = new System.Drawing.Size(57, 20);
             this.lbMessage.TabIndex = 0;
@@ -63,15 +66,33 @@
             this.button2.Text = "Нет";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbTimer
+            // 
+            this.lbTimer.AutoSize = true;
+            this.lbTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lbTimer.Location = new System.Drawing.Point(152, 166);
+            this.lbTimer.Name = "lbTimer";
+            this.lbTimer.Size = new System.Drawing.Size(19, 20);
+            this.lbTimer.TabIndex = 3;
+            this.lbTimer.Text = "0";
+            // 
             // fMessage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(361, 450);
+            this.Controls.Add(this.lbTimer);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.lbMessage);
             this.Name = "fMessage";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.fMessage_FormClosed);
+            this.Load += new System.EventHandler(this.fMessage_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,5 +102,7 @@
         private System.Windows.Forms.Button button1;
         public System.Windows.Forms.Button button2;
         public System.Windows.Forms.Label lbMessage;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbTimer;
     }
 }
--- GuessMelody/GuessMelody/fMessage.cs
+++ GuessMelody/GuessMelody/fMessage.cs
@@ -7,14 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace GuessMelody
 {
     public partial class fMessage : Form
     {
+        int timeAnswer = 10;
         public fMessage()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timeAnswer--;
+            lbTimer.Text = timeAnswer.ToString();
+            if(timeAnswer==0)
+            {
+                timer1.Stop();
+
+            }
+        }
+
+        private void fMessage_Load(object sender, EventArgs e)
+        {
+            timeAnswer = 10;
+            timer1.Start();
+            SoundPlayer sp = new SoundPlayer("Resources\\Alarm01.wav");
+            sp.Play();
+        }
+
+        private void fMessage_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            timer1.Stop();
         }
     }
 }
